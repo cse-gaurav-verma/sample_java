6004a04b415490d72ba95e5e94248386fc127570
gaurav <cse.gaurav.verma@gmail.com>
2026-05-22 14:28:04 +0530

    Edited 100

--- Test.java
+++ Test.java
@@ -2,8 +2,8 @@ public class Test {
 
   static void add(){
 
-    int a=100;
-    int b=100;
+    int a=200;
+    int b=200;
     int result = a*b;
     System.out.println("The sum is " +result);
     
